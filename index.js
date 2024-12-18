ee1e1702902922330e7a9d324f154173183d63d8
rodirgo <rodrigo@chavez.com>
2024-12-18 14:32:07 -0300

    feat: add basic chat, show author, message and read channel by argv

---
+++ index.js
@@ -0,0 +1,11 @@
+import { createClient } from "@retconned/kick-js";
+import chalk from "chalk";
+import util from "util";
+
+let channel = process.argv[2]
+const client = createClient(channel, { logger: false, readOnly: true });
+
+client.on("ChatMessage", async (message) => {
+  console.log(util.format("『%s 』", chalk.hex(message.sender.identity.color)(message.sender.username)))
+  console.log(`${message.content}`);
+});
