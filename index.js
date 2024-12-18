9d5f7430f1848f67ba25bf5b673d66f4b77bb613
rodirgo <rodrigo@chavez.com>
2024-12-18 17:20:37 -0300

    chore(index.js): add sheebang and make it executable

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { createClient } from "@retconned/kick-js";
 import chalk from "chalk";
 import util from "util";
@@ -8,4 +10,5 @@ const client = createClient(channel, { logger: false, readOnly: true });
 client.on("ChatMessage", async (message) => {
   console.log(util.format("『%s 』", chalk.hex(message.sender.identity.color)(message.sender.username)))
   console.log(`${message.content}`);
+  console.log()
 });
